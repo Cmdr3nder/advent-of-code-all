da2ad0e1e0615876e22e3b4550e3941ede9cf435
Andrew Bowers <andrew.bowers@agamecoder.com>
2023-05-07 23:40:39 -0700

    2015-10 complete

---
+++ src/year_2015/day10.rs
@@ -0,0 +1,46 @@
+use std::fs;
+
+use anyhow::Result;
+
+use crate::day::Day;
+
+pub struct Day10;
+
+impl Day for Day10 {
+    fn main() -> Result<()> {
+        let mut sequence: Vec<char> = fs::read_to_string("input/2015/day10.txt")?
+            .trim()
+            .chars()
+            .collect();
+        let mut forty_len = 0;
+        let mut fifty_len = 0;
+        for i in 1..=50 {
+            let mut new_sequence = Vec::new();
+            {
+                // lock mutability temporarily for this loop sequence since I had a bug related to appending to the wrong sequenc
+                let sequence = sequence;
+                let mut j = 0;
+                while j < sequence.len() {
+                    let ch = sequence[j];
+                    let mark = j;
+                    while j < sequence.len() && sequence[j] == ch {
+                        j += 1;
+                    }
+                    for num_char in (j - mark).to_string().chars() {
+                        new_sequence.push(num_char);
+                    }
+                    new_sequence.push(ch);
+                }
+            }
+            sequence = new_sequence;
+            if i == 40 {
+                forty_len = sequence.len();
+            } else if i == 50 {
+                fifty_len = sequence.len();
+            }
+        }
+        println!("Length @ 40th iteration: {forty_len}");
+        println!("Length @ 50th iteration: {fifty_len}");
+        Ok(())
+    }
+}
