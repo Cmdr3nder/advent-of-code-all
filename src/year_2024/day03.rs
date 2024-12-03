97488361633f1c4e446d2f9b7e07f08de575cf55
Andrew Bowers <andrew.bowers@agamecoder.com>
2024-12-03 10:40:27 -0800

    Day 03 Part 02

--- src/year_2024/day03.rs
+++ src/year_2024/day03.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use lazy_regex::regex;
 
 use crate::day::Day;
@@ -10,15 +10,41 @@ pub struct Day03;
 impl Day for Day03 {
     fn main() -> Result<()> {
         let input = fs::read_to_string("input/2024/day03.txt")?;
-        let re = regex!(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)");
+        let re = regex!(r"((mul)\(([0-9]{1,3}),([0-9]{1,3})\))|((do|don't)\(\))");
         let mut total_mult_sum = 0;
+        let mut total_cond_mult_sum = 0;
+        let mut cond = true;
         for match_group in re.captures_iter(&input) {
-            let (match_a, match_b) = match_group.get(1).zip(match_group.get(2)).with_context(|| "A match group was not in expected form.")?;
-            let num_a: u32 = match_a.as_str().parse()?;
-            let num_b: u32 = match_b.as_str().parse()?;
-            total_mult_sum += num_a * num_b;
+            match match_group
+                .get(2)
+                .or(match_group.get(6))
+                .map(|m| m.as_str())
+                .with_context(|| "A match group label was not in expected form.")?
+            {
+                "mul" => {
+                    let (match_a, match_b) = match_group
+                        .get(3)
+                        .zip(match_group.get(4))
+                        .with_context(|| "A mul match group was not in expected form.")?;
+                    let num_a: u32 = match_a.as_str().parse()?;
+                    let num_b: u32 = match_b.as_str().parse()?;
+                    let mult = num_a * num_b;
+                    total_mult_sum += mult;
+                    if cond {
+                        total_cond_mult_sum += mult;
+                    }
+                }
+                "do" => {
+                    cond = true;
+                }
+                "don't" => {
+                    cond = false;
+                }
+                _ => bail!("Unexpected command label"),
+            }
         }
         println!("Total mult command sum: {total_mult_sum}");
+        println!("Total conditional mult command sum: {total_cond_mult_sum}");
         Ok(())
     }
 }
