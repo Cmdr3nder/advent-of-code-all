1958007a767d7d8e88391cb1176a96e50ec5c6ce
Andrew Bowers <andrew.bowers@agamecoder.com>
2024-12-04 09:03:50 -0800

    2024-04 Part 02

--- src/year_2024/day04.rs
+++ src/year_2024/day04.rs
@@ -30,7 +30,15 @@ impl XmasCounter {
         self.cache[3] = ch;
 
         // Examine for 'XMAS'
-        if (self.cache[0] == 'X' && self.cache[1] == 'M' && self.cache[2] == 'A' && self.cache[3] == 'S') || (self.cache[3] == 'X' && self.cache[2] == 'M' && self.cache[1] == 'A' && self.cache[0] == 'S') {
+        if (self.cache[0] == 'X'
+            && self.cache[1] == 'M'
+            && self.cache[2] == 'A'
+            && self.cache[3] == 'S')
+            || (self.cache[3] == 'X'
+                && self.cache[2] == 'M'
+                && self.cache[1] == 'A'
+                && self.cache[0] == 'S')
+        {
             self.count += 1;
         }
     }
@@ -67,7 +75,8 @@ impl Day for Day04 {
             counter.reset_cache();
         }
         // ->South East Diagonal Check (+ ->North West)
-        for x_start in 0..x_len { // Top
+        for x_start in 0..x_len {
+            // Top
             let mut x = x_start;
             let mut y = 0;
             while x < x_len && y < y_len {
@@ -77,7 +86,8 @@ impl Day for Day04 {
             }
             counter.reset_cache();
         }
-        for y_start in 1..y_len { // Left-Side Skipping 0,0 common start
+        for y_start in 1..y_len {
+            // Left-Side Skipping 0,0 common start
             let mut y = y_start;
             let mut x = 0;
             while x < x_len && y < y_len {
@@ -88,7 +98,8 @@ impl Day for Day04 {
             counter.reset_cache();
         }
         // ->South West Diagonal Check (+ ->North East)
-        for x_start in 0..x_len { // Top
+        for x_start in 0..x_len {
+            // Top
             let mut x = x_start;
             let mut y = 0;
             while y < y_len {
@@ -101,7 +112,8 @@ impl Day for Day04 {
             }
             counter.reset_cache();
         }
-        for y_start in 1..y_len { // Right-Side Skipping x_len-1,0 common start
+        for y_start in 1..y_len {
+            // Right-Side Skipping x_len-1,0 common start
             let mut y = y_start;
             let mut x = x_len - 1;
             while y < y_len {
@@ -115,6 +127,23 @@ impl Day for Day04 {
             counter.reset_cache();
         }
         println!("XMAS Instances: {}", counter.count);
+        let mut xmas_count = 0;
+        for x in 1..(x_len - 1) {
+            for y in 1..(y_len - 1) {
+                if mat[y][x] == 'A' {
+                    let nw = mat[y - 1][x - 1];
+                    let ne = mat[y - 1][x + 1];
+                    let sw = mat[y + 1][x - 1];
+                    let se = mat[y + 1][x + 1];
+                    let d1 = (nw == 'M' && se == 'S') || (nw == 'S' && se == 'M');
+                    let d2 = (ne == 'M' && sw == 'S') || (ne == 'S' && sw == 'M');
+                    if d1 && d2 {
+                        xmas_count += 1;
+                    }
+                }
+            }
+        }
+        println!("X-MAS Instances: {xmas_count}");
         Ok(())
     }
 }
