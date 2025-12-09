1ebe1ac33a94e71d7196e7dc4d968bd815904c42
Andrew Bowers <andrew.bowers@agamecoder.com>
2025-12-09 08:56:26 -0800

    Day 04, part 2 complete

--- src/year_2025/day04.rs
+++ src/year_2025/day04.rs
@@ -1,98 +1,85 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
+use std::collections::HashSet;
 use std::io::{BufRead, BufReader};
 
+use crate::data::Point2D;
 use crate::day::Day;
 use crate::util::input::get_input;
 
 pub struct Day04;
 
-fn count_available(current_row: &Vec<char>, prev_row: &Vec<char>, next_row: &Vec<char>) -> u64 {
-    let mut available_count = 0;
-    for (x, ch) in current_row.iter().enumerate() {
-        let ch = *ch;
-        if ch == '@' {
-            let mut neighbor_count = 0;
+type Point = Point2D<usize>;
+type Stacks = HashSet<Point>;
+type Removable = Vec<Point>;
+type Extents = (Point, Point);
 
-            // Check (x - 1 column)
-            if x > 0 {
-                let check_x = x - 1;
-                if check_x < prev_row.len() && prev_row[check_x] == '@' {
-                    neighbor_count += 1;
+fn is_removable(stacks: &Stacks, location: &Point) -> bool {
+    let low_y = if location.y > 0 { location.y - 1 } else { 0 };
+    let high_y = location.y + 1;
+    let low_x = if location.x > 0 { location.x - 1 } else { 0 };
+    let high_x = location.x + 1;
+    let mut neighbor_count = 0;
+    'removable_check: for y in low_y..=high_y {
+        for x in low_x..=high_x {
+            let neighbor = &Point::new(x, y);
+            if neighbor != location && stacks.contains(neighbor) {
+                neighbor_count += 1;
+                if neighbor_count >= 4 {
+                    break 'removable_check;
                 }
-                if check_x < current_row.len() && current_row[check_x] == '@' {
-                    neighbor_count += 1;
-                }
-                if check_x < next_row.len() && next_row[check_x] == '@' {
-                    neighbor_count += 1;
-                }
-            }
-
-            // Check (x column)
-            {
-                if x < prev_row.len() && prev_row[x] == '@' {
-                    neighbor_count += 1;
-                }
-                if x < next_row.len() && next_row[x] == '@' {
-                    neighbor_count += 1;
-                }
-            }
-
-            // Check (x + 1 column)
-            {
-                let check_x = x + 1;
-                if check_x < prev_row.len() && prev_row[check_x] == '@' {
-                    neighbor_count += 1;
-                }
-                if check_x < current_row.len() && current_row[check_x] == '@' {
-                    neighbor_count += 1;
-                }
-                if check_x < next_row.len() && next_row[check_x] == '@' {
-                    neighbor_count += 1;
-                }
-            }
-
-            if neighbor_count < 4 {
-                available_count += 1;
             }
         }
     }
-    available_count
+    return neighbor_count < 4;
+}
+
+fn find_removable(stacks: &Stacks, extents: &Extents) -> Removable {
+    let mut removable = Removable::new();
+    for y in extents.0.y..=extents.1.y {
+        for x in extents.0.x..=extents.1.x {
+            let location = Point2D::new(x, y);
+            if stacks.contains(&location) && is_removable(stacks, &location) {
+                removable.push(location);
+            }
+        }
+    }
+    removable
 }
 
 impl Day for Day04 {
     fn main() -> Result<()> {
         let input = BufReader::new(get_input(2025, 4)?);
 
-        let empty_row: Vec<char> = Vec::new();
-        let mut recent_rows: Vec<Vec<char>> = Vec::with_capacity(3);
-
-        let mut available_count = 0;
-        for row in input.lines().map(|l| l.unwrap()) {
-            if recent_rows.len() == 3 {
-                // Drop oldest row
-                recent_rows.remove(0);
+        let mut stacks: Stacks = Stacks::new();
+        let mut extents: Extents = (Point::new(0, 0), Point::new(0, 0));
+        for (y, row) in input.lines().map(|l| l.unwrap()).enumerate() {
+            if y > extents.1.y {
+                extents.1.y = y;
             }
-            // Add newest row
-            recent_rows.push(row.chars().collect());
-            match recent_rows.len() {
-                1 => {
-                    // Do nothing, we can't look ahead yet.
-                },
-                2 => {
-                    // Special case, look at 'first' row while checking the second row.
-                    available_count += count_available(&recent_rows[0], &empty_row, &recent_rows[1]);
-                },
-                3 => {
-                    // Common case, look at 'second' row while checking the 'first' and 'third' rows.
-                    available_count += count_available(&recent_rows[1], &recent_rows[0], &recent_rows[2]);
-                },
-                _ => bail!("Unexpected count for recent_rows, expected there to be at least 1 row and no more than 3"),
+            for (x, ch) in row.chars().enumerate() {
+                if x > extents.1.x {
+                    extents.1.x = x;
+                }
+                if ch == '@' {
+                    stacks.insert(Point::new(x, y));
+                }
             }
         }
-        // Process last row
-        available_count += count_available(&recent_rows[2], &recent_rows[1], &empty_row);
+        // Proccess first pass
+        let mut removable = find_removable(&stacks, &extents);
+        let available_count = removable.len();
+        let mut total_removed = 0;
+        while removable.len() > 0 {
+            total_removed += removable.len();
+            // Apply removal
+            for r in removable {
+                stacks.remove(&r);
+            }
+            removable = find_removable(&stacks, &extents);
+        }
 
         println!("Available rolls to move: {available_count}");
+        println!("Removed rolls: {total_removed}");
 
         Ok(())
     }
