d8cf1f00006e1ba05949089456a3a2ee2bb164e3
Andrew Bowers <andrew.bowers@agamecoder.com>
2025-12-27 16:11:06 -0800

    Day 09, part 1 simplified

--- src/year_2025/day09.rs
+++ src/year_2025/day09.rs
@@ -1,7 +1,6 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
 use lazy_regex::regex_captures;
-use std::cmp::Ordering;
-use std::collections::HashSet;
+use std::cmp::{Ordering, PartialOrd};
 use std::io::{BufRead, BufReader};
 use std::u64;
 
@@ -14,137 +13,26 @@ struct Point {
     y: u64,
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Area {
-    area: u64,
-    low: Point,
-    high: Point,
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Ord, Hash)]
+struct Rectangle {
     a: Point,
     b: Point,
 }
 
-impl Point {
-    pub fn rect_area(&self, other: &Self) -> Area {
-        Area {
-            area: (self.x.abs_diff(other.x) + 1) * (self.y.abs_diff(other.y) + 1),
-            low: Point {
-                x: if self.x < other.x { self.x } else { other.x },
-                y: if self.y < other.y { self.y } else { other.y },
-            },
-            high: Point {
-                x: if self.x > other.x { self.x } else { other.x },
-                y: if self.y > other.y { self.y } else { other.y },
-            },
-            a: *self,
-            b: *other,
-        }
+impl Rectangle {
+    fn area(&self) -> u64 {
+        (self.a.x.abs_diff(self.b.x) + 1) * (self.a.y.abs_diff(self.b.y) + 1)
     }
 }
 
-enum PenState {
-    FindEdge,      // Pen up
-    FoundEdge,     // Pen up, but TBD
-    TraceEdge,     // Pen up, wait for next gap to FindEdge
-    TraceContents, // Pen down, wait for next edge to FoundEdge
-}
-
-struct Polygon {
-    first_point: Option<Point>,
-    last_point: Option<Point>,
-    outline: HashSet<Point>,
-    finished: bool,
-    y_min: u64,
-    y_max: u64,
-    x_min: u64,
-    x_max: u64,
-}
-
-impl Polygon {
-    fn new() -> Self {
-        Polygon {
-            first_point: None,
-            last_point: None,
-            outline: HashSet::new(),
-            finished: false,
-            y_min: u64::MAX,
-            y_max: 0,
-            x_min: u64::MAX,
-            x_max: 0,
-        }
-    }
-
-    fn insert(&mut self, new_tile: Point) -> Result<()> {
-        if self.finished {
-            bail!("Polygon already finished, cannot insert {new_tile:?}");
-        }
-        if self.first_point == None {
-            self.first_point = Some(new_tile);
-        }
-        // Draw Outline
-        if let Some(prev) = self.last_point {
-            match (prev.x.cmp(&new_tile.x), prev.y.cmp(&new_tile.y)) {
-                (Ordering::Less | Ordering::Greater, Ordering::Equal) => {
-                    // Horizontal
-                    for x in new_tile.x..prev.x {
-                        self.outline.insert(Point { x, y: prev.y });
-                    }
-                }
-                (Ordering::Equal, Ordering::Less | Ordering::Greater) => {
-                    // Vertical
-                    for y in new_tile.y..prev.y {
-                        self.outline.insert(Point { x: prev.x, y });
-                    }
-                }
-                _ => bail!("Unexpected point pair for outlining, ({prev:?}, {new_tile:?})"),
-            }
-        } else {
-            self.outline.insert(new_tile);
-        }
-        // Find Extents
-        if new_tile.x < self.x_min {
-            self.x_min = new_tile.x;
-        } else if new_tile.x > self.x_max {
-            self.x_max = new_tile.x;
-        }
-        if new_tile.y < self.y_min {
-            self.y_min = new_tile.y;
-        } else if new_tile.y > self.y_max {
-            self.y_max = new_tile.y;
-        }
-        // Prep for next insert
-        self.last_point = Some(new_tile);
-        Ok(())
-    }
-
-    fn finish(&mut self) -> Result<()> {
-        if self.finished {
-            bail!("Polygon already finished, cannot finish twice");
-        } else {
-            if let Some(first_point) = self.first_point {
-                // Finish outline
-                self.insert(first_point)?;
-                // Finished
-                self.finished = true;
-                Ok(())
-            } else {
-                bail!("Polygon has no points, cannot finish yet");
-            }
-        }
-    }
-
-    fn safe(&self, area: &Area) -> Result<bool> {
-        if !self.finished {
-            bail!("Polygon must be finished before doing safe checks");
-        }
-        for y in area.low.y..=area.high.y {
-            for x in self.x_min..=area.high.x {
-                let p = Point { x, y };
-                if !self.outline.contains(&p) {
-                    return Ok(false);
-                }
-            }
-        }
-        Ok(true)
+impl PartialOrd for Rectangle {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(
+            self.area()
+                .cmp(&other.area())
+                .then(self.a.cmp(&other.a))
+                .then(self.b.cmp(&other.b)),
+        )
     }
 }
 
@@ -156,8 +44,8 @@ impl Day for Day09 {
 
         // Parse Points & Measure Basic Areas
         let mut red_tiles: Vec<Point> = Vec::new();
-        let mut areas: Vec<Area> = Vec::new();
-        let mut polygon = Polygon::new();
+        let mut outline: Vec<Rectangle> = Vec::new();
+        let mut red_zones: Vec<Rectangle> = Vec::new();
         for (line_num, line) in input.lines().map(|l| l.unwrap()).enumerate() {
             let (_, x_str, y_str) = regex_captures!(r"([0-9]+),([0-9]+)", &line)
                 .with_context(|| format!("No point on line {line_num}"))?;
@@ -165,26 +53,39 @@ impl Day for Day09 {
                 x: x_str.parse()?,
                 y: y_str.parse()?,
             };
+            // Spawn red cornered rectangles
             for tile in &red_tiles {
-                areas.push(new_tile.rect_area(tile));
+                red_zones.push(Rectangle {
+                    a: new_tile,
+                    b: *tile,
+                });
             }
-            polygon.insert(new_tile)?;
+            // Draw outline
+            if let Some(prev) = red_tiles.last() {
+                outline.push(Rectangle {
+                    a: *prev,
+                    b: new_tile,
+                });
+            }
             red_tiles.push(new_tile);
         }
 
-        areas.sort_unstable();
-        areas.reverse();
-
-        println!("Largest area based on red tiles, {}", areas[0].area,);
-
-        polygon.finish()?;
-
-        for area in areas {
-            if polygon.safe(&area)? {
-                println!("Largest christmas area, {}", area.area);
-                break;
-            }
+        // Complete outline
+        if let Some((first, last)) = red_tiles.first().zip(red_tiles.last()) {
+            outline.push(Rectangle {
+                a: *last,
+                b: *first,
+            });
         }
+
+        // Sort red cornered rectangles
+        red_zones.sort_unstable();
+        red_zones.reverse();
+
+        println!(
+            "Largest area based on red tiles, {}",
+            red_zones.first().with_context(|| "No Red Zones?!")?.area()
+        );
 
         Ok(())
     }
